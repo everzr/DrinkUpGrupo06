1909c733d8a91f580118b3287b4b369d393bb0ef
Ever Zamora <ever.zamora@catolica.edu.sv>
2025-05-24 17:17:05 -0600

    Better Design

--- app/src/main/java/sv/edu/catolica/drinkupgrupo06/RegistrarConsumoActivity.java
+++ app/src/main/java/sv/edu/catolica/drinkupgrupo06/RegistrarConsumoActivity.java
@@ -40,7 +40,7 @@ public class RegistrarConsumoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registrar_consumo);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, v.getPaddingTop(), systemBars.right, systemBars.bottom);
             return insets;
         });
 
